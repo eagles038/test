b016bfa28d03abd5f3aa49778b01496ca1f49c0b
Ruslan <bahamak463@cyberper.net>
2020-09-14 13:39:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// const result = confirm("Are you here");
+
+// const answer = prompt("Вам 18 лет?", '50');
+
+// console.log(typeof +answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Your name?', '');
+// answers[1] = prompt('Your family?', '');
+// answers[3] = prompt('Your age?', '');
+
+// document.write(answers);
+
+const user = 'Ivan';
+
+console.log(`ПРивет ${user}`);
